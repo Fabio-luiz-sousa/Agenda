4dbbe5d6cd5d55be3d59684b780b752e1a0f3e4c
Fábio Luiz Sousa <fabioprogramar@gmail.com>
2022-08-08 10:51:36 -0300

    atualização do gitignore

--- agenda/settings.py
+++ agenda/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from django.contrib.messages import constants
 from pathlib import Path
+from .senhas import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dxt^5)h%(ttmnz+m590ep^&gx1(4jq6n29%5!do5=a^tvy(!$x'
+SECRET_KEY = f'{secret_key}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
